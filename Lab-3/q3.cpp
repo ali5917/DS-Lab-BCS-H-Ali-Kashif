67547a15be43f88ce472ba4153da1614e3989241
Ali Kashif <alikashif5917@gmail.com>
2025-09-07 05:47:50 +0000

    reverse nodes in k group

--- Lab-3/q3.cpp
+++ Lab-3/q3.cpp
@@ -14,6 +14,32 @@ class Node {
         Node (int val) : value (val), next(nullptr) {}
 };
 
-Node* mergeSortedLists (Node *head, int k {
+Node* reverseKGroup (Node *head, int k) {
+    Node *newHead = nullptr; // new start of the linked list
+    Node *prevTail = nullptr;
     
+    Node *p = head;
+    while (p) {
+        // reversing k nodes
+        Node *prev = nullptr, *curr = p, *next = nullptr;
+        Node *startK = curr;
+        for (int i = 0; i < k && curr; i++) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        if (!newHead) {
+            newHead = prev;
+        }
+        
+        // connecting prev reversed group to new reversed group
+        if (prevTail) prevTail->next = prev;
+        prevTail = startK;
+        
+        // moving to next group
+        startK->next = curr;
+        p = curr;
+    }
+    return newHead;
 }
